ba6903b9de4b46707c248d6f0cd8d2830cf46c23
Elisa <ciardulli.elisa@gmail.com>
2025-05-06 13:36:33 +0200

    create Spring boot project

---
+++ src/main/java/unibz/joel/ParkingBuddy/ParkingBuddyApplication.java
@@ -0,0 +1,13 @@
+package unibz.joel.ParkingBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unibz/joel/ParkingBuddy/ParkingBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package unibz.joel.ParkingBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
